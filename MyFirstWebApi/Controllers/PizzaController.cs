e1e98dbb6730b3062c899d40ca3d9d1fe900cef5
Ben Harrington <harringtonben@icloud.com>
2018-03-24 10:41:38 -0500

    created first route and post for API

---
+++ MyFirstWebApi/Controllers/PizzaController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MyFirstWebApi.Controllers
+{
+    [RoutePrefix("orderapi/pizza")]
+    public class PizzaController : ApiController
+    {
+        [HttpPost,Route("{id}")]
+        public HttpResponseMessage PlaceOrder(int id)
+        {
+            return Request.CreateResponse(HttpStatusCode.Created, 5);
+        }
+    }
+}
