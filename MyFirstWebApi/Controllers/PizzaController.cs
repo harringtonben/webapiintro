3fd09e0fae511efd5d6c0422fe06546260f4d3a1
Ben Harrington <harringtonben@icloud.com>
2018-03-24 11:23:24 -0500

    added dapper and connected to SQL for get calls

--- MyFirstWebApi/Controllers/PizzaController.cs
+++ MyFirstWebApi/Controllers/PizzaController.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Dapper;
+using MyFirstWebApi.Models;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -13,7 +17,38 @@ namespace MyFirstWebApi.Controllers
         [HttpPost,Route("{id}")]
         public HttpResponseMessage PlaceOrder(int id)
         {
+            if (id == 10)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "No order with that id was found");
+            }
             return Request.CreateResponse(HttpStatusCode.Created, 5);
+        }
+
+        [HttpGet,Route("")]
+        public HttpResponseMessage GetAllOrders()
+        {
+            using (var db = new SqlConnection(ConfigurationManager.ConnectionStrings["PizzaTime"].ConnectionString))
+            {
+                db.Open();
+
+                var orders = db.Query<Order>("select * from Orders");
+
+                return Request.CreateResponse(HttpStatusCode.OK, orders);
+
+            }
+        }
+
+        [HttpGet, Route("{id}")]
+        public HttpResponseMessage GetOrder(int id)
+        {
+            using (var db = new SqlConnection(ConfigurationManager.ConnectionStrings["PizzaTime"].ConnectionString))
+            {
+                db.Open();
+
+                var order = db.QueryFirst<Order>("select * from Orders where Id = @id", new {id});
+
+                return Request.CreateResponse(HttpStatusCode.OK, order); 
+            }
         }
     }
 }
---
+++ MyFirstWebApi/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyFirstWebApi.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string TypeOfPizza { get; set; }
+        public int NumberOfPizzas { get; set; }
+        public double Cost { get; set; }
+        public string AddressForDelivery { get; set; }
+        public string NameOfCustomer { get; set;}
+    }
+}
